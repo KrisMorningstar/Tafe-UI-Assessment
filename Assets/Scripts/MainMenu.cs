c217767284716dcc7112a4977f9b783023e6d7c6
KrisMorningstar <109941232+KrisMorningstar@users.noreply.github.com>
2023-04-28 04:08:59 +0800

    Sound Features Added

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,6 +7,8 @@ using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    private int toggleStatus = 0;
+
     public void MenuChange(GameObject _menu)
     {
         EventSystem.current.currentSelectedGameObject.transform.parent.gameObject.SetActive(false);
@@ -16,5 +18,27 @@ public class MainMenu : MonoBehaviour
     public void SliderAdjust(TMP_Text _value)
     {
         _value.text = EventSystem.current.currentSelectedGameObject.GetComponent<Slider>().value.ToString();
+    }
+
+    public void ToggleSwitch(GameObject _light)
+    {
+        Color _off = new Color(0.2f, 0.07f, 0.07f, 1);
+        Color _on = new Color(0.8f, 0.07f, 0.07f, 1);
+        Color _half = new Color(0.4f, 0.07f, 0.07f, 1);
+        switch (toggleStatus)
+        {
+            case 0:
+                _light.GetComponent<Image>().color = _on;
+                toggleStatus = 1;
+                break;
+            case 1:
+                _light.GetComponent<Image>().color = _half;
+                toggleStatus = 2;
+                break;
+            case 2:
+                _light.GetComponent<Image>().color = _off;
+                toggleStatus = 0;
+                break;
+        }
     }
 }
